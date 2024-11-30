e6f6ae722f20d8fa053f419fd148811ae3e8efb9
alexei shumilov <a.shumilau@astondevs.ru>
2024-11-30 11:28:53 +0300

    Решена задача с выводом таблицы умножения

--- src/main/java/by/aston/Main.java
+++ src/main/java/by/aston/Main.java
@@ -2,6 +2,11 @@ package by.aston;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
     }
 }
