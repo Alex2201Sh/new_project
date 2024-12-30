12ea1bcbd62a468619c59f054091c27d487f9f17
Alexei Shumilov <shumilov218@gmail.com>
2024-12-30 20:12:23 +0300

    Добавил код с занятия №13 с примерами тестов

---
+++ src/main/java/by/aston/lesson13/MainClass.java
@@ -0,0 +1,9 @@
+package by.aston.lesson13;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sum = calculator.sum(0, 0);
+        System.out.println(sum);
+    }
+}
