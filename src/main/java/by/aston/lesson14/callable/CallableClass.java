9e3ccb8d0f579ef66ed3ce63069c9789b9963a1f
Alexei Shumilov <shumilov218@gmail.com>
2025-01-18 09:17:36 +0300

    Добавил код с занятия №14 - Примеры многопоточного программирования

---
+++ src/main/java/by/aston/lesson14/callable/CallableClass.java
@@ -0,0 +1,17 @@
+package by.aston.lesson14.callable;
+
+import java.util.concurrent.Callable;
+
+class CallableClass implements Callable<String> {
+    private final int threadNum;
+
+    public CallableClass(int threadNum) {
+        this.threadNum = threadNum;
+    }
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(2000L * threadNum);
+        return Thread.currentThread().getName();
+    }
+}
---
+++ src/main/java/by/aston/lesson14/wait_notify/Consumer.java
@@ -0,0 +1,18 @@
+package by.aston.lesson14.wait_notify;
+
+// Класс Потребитель
+public class Consumer implements Runnable {
+
+    private Store store;
+
+    public Consumer(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            store.get();
+        }
+    }
+}
