76eff4d180afcf6e9475e62b6908db73d2b1115a
Alexei Shumilov <shumilov218@gmail.com>
2025-01-30 18:30:45 +0300

    Добавил 2 задачи на компрессию строки и поиск чисел дающих заданную сумму

---
+++ src/main/java/by/aston/livecoding/Algo6StringCompression.java
@@ -0,0 +1,7 @@
+package by.aston.livecoding;
+
+public class Algo6StringCompression {
+    //дано: строка "aaaabbbcccccooooooddddddddddddddz"
+    //необходимо схать строку по типу буква-количество повторений.
+    //результат: "a4b3c5o6d14z"
+}
---
+++ src/main/java/by/aston/livecoding/Algo7FindSum.java
@@ -0,0 +1,8 @@
+package by.aston.livecoding;
+
+public class Algo7FindSum {
+    //дано
+    //int[]array = new int[]{5,7,2,3};
+    //int target = 9;
+    //найти пару чисел из array, которые в сумме дают target
+}
